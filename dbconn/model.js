c0f710cf56fc1251ae0f8b82031f15ce030e9dba
Muhammad Jahanzaib <53260475+jahanzaibsheikh830@users.noreply.github.com>
2022-04-13 22:28:53 +0500

    Update model.js

--- dbconn/model.js
+++ dbconn/model.js
@@ -1,7 +1,6 @@
 var mongoose = require("mongoose");
 
-let dbURI = "mongodb+srv://dbjahan:dbjahan@cluster0.8ric4.mongodb.net/test?retryWrites=true&w=majority";
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true });
+let dbURI = 
 
 mongoose.connection.on('connected', function () {
     console.log("Mongoose is connected");
